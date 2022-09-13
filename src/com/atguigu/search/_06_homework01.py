550bbc0790eee9e5d2c65b7da9c5051f4e733df4
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-14 07:47:02 +0800

    AM 01st commit

--- src/com/atguigu/search/_06_homework01.py
+++ src/com/atguigu/search/_06_homework01.py
@@ -7,6 +7,31 @@
 # Author==>02雪乃赤瞳楪祈校条祭制作委员会
 # Software==>PyCharm
 
+import requests
+
+from bs4 import BeautifulSoup
+
+result = requests.request(method='get',url='https://www.shanghairanking.cn/rankings/bcur/2022')
+
+print(result.status_code)
+
+print(result.ok)
+
+result.encoding = 'utf-8'
+
+soup = BeautifulSoup(markup=result.text,features='html.parser')
+rank = 0
+print('排名    学校名称   省市    总分')
+print()
+for university in soup.select('.name-cn'):
+    rank += 1
+    print(rank,'    ',university.text.strip(),'    ',)
+
+
+
+
+
+
 
 
 
