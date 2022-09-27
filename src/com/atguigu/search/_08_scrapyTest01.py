6e02b2b028a6e870bec6c217473749c0d6c63257
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-27 08:42:28 +0800

    AM 01st commit

---
+++ src/com/atguigu/search/_08_scrapyTest01.py
@@ -0,0 +1,10 @@
+# -*- coding = utf-8 -*-
+# Description==>TODO
+# BelongsProject==>search_basic
+# BelongsPackage==>
+# CreateTime==>2022-09-27 08:42:19
+# Version==>1.0
+# Author==>02雪乃赤瞳楪祈校条祭制作委员会
+# Software==>PyCharm
+
+
