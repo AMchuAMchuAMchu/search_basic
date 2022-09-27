85bdc4ca944816b58fc64a61998901c55ed68ac9
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-27 09:42:19 +0800

    AM 01st commit

--- src/com/atguigu/scarpy/__init__.py
+++ src/com/atguigu/scarpy/__init__.py
@@ -2,8 +2,7 @@
 # Description==>TODO
 # BelongsProject==>search_basic
 # BelongsPackage==>
-# CreateTime==>2022-09-27 08:42:19
+# CreateTime==>2022-09-27 09:38:30
 # Version==>1.0
 # Author==>02雪乃赤瞳楪祈校条祭制作委员会
 # Software==>PyCharm
-
