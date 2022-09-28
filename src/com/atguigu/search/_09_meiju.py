023876cb66c5fc7d0b20ee63ec8bb8789a352696
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-28 22:04:39 +0800

    AM

---
+++ src/com/atguigu/search/_09_meiju.py
@@ -0,0 +1,23 @@
+# Description ==> TODO
+# BelongsProject ==> search_basic
+# BelongsPackage ==> 
+# Version ==> 1.0
+# CreateTime ==> 2022-09-28 21:54:37
+# Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+
+import requests
+
+from bs4 import BeautifulSoup
+
+result = requests.request(method='get',url='https://www.meiju56.com/')
+
+print(result.status_code)
+
+movies = BeautifulSoup(result.text,features='html.parser')
+
+for item in movies.select('.module-item'):
+    print(item.text)
+
+
+
+
