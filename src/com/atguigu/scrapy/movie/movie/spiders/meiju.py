e09bb7ddcf7d8e55581a99206ea42b5175794269
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-15 18:50:36 +0800

    AM 01st commit

--- src/com/atguigu/scrapy/movie/movie/spiders/meiju.py
+++ src/com/atguigu/scrapy/movie/movie/spiders/meiju.py
@@ -6,18 +6,17 @@ class MeijuSpider(scrapy.Spider):
     start_urls = ['http://meijut.cc/label/news.html']
 
     def parse(self, response):
+        count = 0
         movies = response.xpath('//ul[@class="top-list fn-clear"]/li')
-        print('========')
-        print('========')
-        print('========')
-
-
-        print(movies.extract())
-        print('========')
-        print('========')
-        print('========')
+        # print(movies.extract())
         for each_movie in movies:
-            item = MovieItem()
-            item['name'] = each_movie.xpath('./h5/a/@title').extract()[0]
-            yield item
+            count += 1
+            # item = MovieItem()
+            # item['name'] = each_movie.xpath('./h5/a/@title').extract()[0]
+            res = each_movie.xpath('./h5/a/@title').extract()[0]
+            print(count,'>>',res)
+            # file = open(file=r'D:\seldom\rd\Python_ProjectAll\search_basic\src\com\atguigu\scrapy\movie\meiju.txt',mode='w',encoding='utf-8')
+            # res02 = str(count)+'>>'+res
+            # file.writelines(str(res02))
+            # yield item
 
