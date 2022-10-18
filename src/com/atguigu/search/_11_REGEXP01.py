c576c39b7a73f969cc612c440ee317946b9302f5
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-18 10:30:56 +0800

    AM 01st commit

---
+++ src/com/atguigu/search/_11_REGEXP01.py
@@ -0,0 +1,13 @@
+# Description ==> TODO
+# BelongsProject ==> search_basic
+# BelongsPackage ==> 
+# Version ==> 1.0
+# CreateTime ==> 2022-10-18 10:27:17
+# Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+
+import re
+
+res = re.findall(pattern='((0\d{2}|0\d{3})-(\d{8}|\d{6}))',string='电话号码021-62232333')
+res01 = list(res)
+print(res01[0][0])
+
